cafc0926e8cc57e500e65399b4dec253716c7c1d
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-05-04 12:21:19 -0400

    updated SKB added SKPB

--- BDecks.lua
+++ BDecks.lua
@@ -1,5 +1,7 @@
 --_RELEASE_MODE = false -- REMOVE FOR RELEASE
 
+
+
 SMODS.Back{ -- Made by Bumpy
     name = "Geology Deck",
     key = "geology",
@@ -126,18 +128,26 @@ SMODS.Back{ -- Made by Facade
     name = "Steel King Bonanza",
     key = "steelkingbonanza",
     pos = {x = 6, y = 1},
-    config = {rank = 'King'},
+    config = {rank = 'King', vouchers = {'v_observatory', 'v_telescope'}},
     loc_txt = {
         name = "Steel King Bonanza",
         text = {"All cards are Kings", 
         "All cards have a {C:red}Red Seal{}",  
         "All cards are {X:mult,C:white}Steel{}",
-                "{C:attention}+2{} hand size"}
+        "Starts with {C:tarot,T:v_observatory}Observatory{},",
+        "{C:tarot,T:v_telescope}Telescope{}, {C:red,E:2}Baron{}, and {C:legendary,E:2}Perkeo{}"}
     },
     apply = function(self)
         G.E_MANAGER:add_event(Event({
             func = function()
-                G.hand.config.card_limit = G.hand.config.card_limit + 2
+                local baron_card = create_card("Joker", G.jokers, nil, nil, nil, nil, 'j_baron', 'deck')
+                local perkeo_card = create_card("Joker", G.jokers, nil, nil, nil, nil, 'j_perkeo', 'deck')
+                baron_card:add_to_deck()
+                perkeo_card:add_to_deck()
+                G.jokers:emplace(baron_card)
+                G.jokers:emplace(perkeo_card)
+                baron_card:start_materialize()
+                perkeo_card:start_materialize()
                 for it, card in ipairs(G.playing_cards) do
                     assert(SMODS.change_base(card, nil, self.config.rank))
                     G.playing_cards[it]:set_ability(G.P_CENTERS.m_steel)
@@ -148,3 +158,92 @@ SMODS.Back{ -- Made by Facade
         }))
     end
 }
+
+SMODS.Back{
+    name = "Steel King Plasma Bonanza",
+    key = "steelkingplasmabonanza",
+    pos = {x = 6, y = 1},
+    config = {rank = 'King', vouchers = {'v_observatory', 'v_telescope'}},
+    loc_txt = {
+        name = "Steel King Plasma Bonanza",
+        text = {"Combines the effects of the Steel",
+        "King Bonanza and Plasma Deck.",
+        }
+    },
+    apply = function(self)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                local baron_card = create_card("Joker", G.jokers, nil, nil, nil, nil, 'j_baron', 'deck')
+                local perkeo_card = create_card("Joker", G.jokers, nil, nil, nil, nil, 'j_perkeo', 'deck')
+                baron_card:add_to_deck()
+                perkeo_card:add_to_deck()
+                G.jokers:emplace(baron_card)
+                G.jokers:emplace(perkeo_card)
+                baron_card:start_materialize()
+                perkeo_card:start_materialize()
+
+                for it, card in ipairs(G.playing_cards) do
+                    assert(SMODS.change_base(card, nil, self.config.rank))
+                    G.playing_cards[it]:set_ability(G.P_CENTERS.m_steel)
+                    G.playing_cards[it]:set_seal('Red', true, true)
+                end
+                return true
+            end
+        }))
+    end,
+
+    calculate = function(self, card, context)
+        if context.final_scoring_step then
+            local tot = hand_chips + mult
+            hand_chips = math.floor(tot/2)
+            mult = math.floor(tot/2)
+            update_hand_text({delay = 0}, {mult = mult, chips = hand_chips})
+
+            G.E_MANAGER:add_event(Event({
+                func = (function()
+                    local text = localize('k_balanced')
+                    play_sound('gong', 0.94, 0.3)
+                    play_sound('gong', 0.94*1.5, 0.2)
+                    play_sound('tarot1', 1.5)
+                    ease_colour(G.C.UI_CHIPS, {0.8, 0.45, 0.85, 1})
+                    ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
+                    attention_text({
+                        scale = 1.4, text = text, hold = 2, align = 'cm', offset = {x = 0,y = -2.7},major = G.play
+                    })
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        blockable = false,
+                        blocking = false,
+                        delay =  4.3,
+                        func = (function() 
+                                ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
+                                ease_colour(G.C.UI_MULT, G.C.RED, 2)
+                            return true
+                        end)
+                    }))
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        blockable = false,
+                        blocking = false,
+                        no_delete = true,
+                        delay =  6.3,
+                        func = (function() 
+                            G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] = G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
+                            G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] = G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
+                            return true
+                        end)
+                    }))
+                    return true
+                end)
+            }))
+    
+            delay(0.6)
+
+            return {
+                chips = hand_chips,
+                mult = mult
+            }
+        end
+    end
+}
+
