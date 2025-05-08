1080803d45c4a2ca679439d1c1cc5efd775dd06f
Bumpy <41409776+BumpyAHK@users.noreply.github.com>
2025-05-08 15:43:33 -0700

    Cleanup, New Atlas, and added Smeared Deck

--- decks/BDecks.lua
+++ decks/BDecks.lua
@@ -1,10 +1,9 @@
-init = function()
-	local deckbuilder = Back.apply_to_run
-	function Back.apply_to_run(self)
-		deckbuilder(self)
-	end
-end
-			
+SMODS.Atlas({
+	key = "deckatlas",
+	path = "DeckAtlas.png",
+	px = 71,
+	py = 95,
+})
 
 SMODS.Back{ -- Made by Bumpy
     name = "Geology Deck",
@@ -163,7 +162,7 @@ SMODS.Back{ -- Made by Facade
     end
 }
 
-SMODS.Back{
+SMODS.Back{ -- Made by Facade
     name = "Steel King Plasma Bonanza",
     key = "steelkingplasmabonanza",
     pos = {x = 6, y = 1},
@@ -251,7 +250,7 @@ SMODS.Back{
     end
 }
 
-SMODS.Back{
+SMODS.Back{ -- Made by Facade
     name = "Steel King Plasma Negative Bonanza",
     key = "steelkingplasmanegativebonanza",
     pos = {x = 6, y = 1},
@@ -348,6 +347,113 @@ SMODS.Back{
             return {
                 chips = hand_chips,
                 mult = mult
+            }
+        end
+    end
+}
+
+
+
+SMODS.Back{ -- Made by Bumpy
+    name = "Smeared Deck",
+    key = "smeareddeck",
+	atlas = "deckatlas",
+    pos = {x = 0, y = 0},
+    config = {discards = 1, 
+			dollars = 10, 
+			extra_hand_bonus = 2, 
+			extra_discard_bonus = 1, 
+			no_interest = true, 
+			joker_slot = 1,
+			consumables = {'c_fool', 'c_fool', 'c_hex'}, 
+			consumable_slot = -1, 
+			spectral_rate = 2, 
+			remove_faces = true, 
+			vouchers = {'v_tarot_merchant','v_planet_merchant', 'v_overstock_norm', 'v_crystal_ball', 'v_telescope'}, 
+			ante_scaling = 2, 
+			randomize_rank_suit = true},
+    loc_txt = {
+        name = "Smeared Deck",
+        text ={
+            "Applies Effects of ",
+			"ALL vanilla decks"
+        },
+    },
+    apply = function()
+        G.E_MANAGER:add_event(Event({
+            func = function()
+				for k, v in pairs(G.playing_cards) do
+                    if v.base.suit == 'Clubs' then 
+                        v:change_suit('Spades')
+                    end
+                    if v.base.suit == 'Diamonds' then 
+                        v:change_suit('Hearts')
+                    end
+                end
+                return true
+            end
+        }))
+		
+    end,
+	calculate = function(self, card, context)	
+        if context.final_scoring_step then
+            local tot = hand_chips + mult
+            hand_chips = math.floor(tot/2)
+            mult = math.floor(tot/2)
+            update_hand_text({delay = 0}, {mult = mult, chips = hand_chips})
+			
+			if G.GAME.last_blind and G.GAME.last_blind.boss then
+				G.E_MANAGER:add_event(Event({
+						func = (function()
+							add_tag(Tag('tag_double'))
+							play_sound('generic1', 0.9 + math.random()*0.1, 0.8)
+							play_sound('holo1', 1.2 + math.random()*0.1, 0.4)
+							return true
+						end)
+					}))
+				end			
+
+            G.E_MANAGER:add_event(Event({
+                func = (function()
+                    local text = localize('k_balanced')
+                    play_sound('gong', 0.94, 0.3)
+                    play_sound('gong', 0.94*1.5, 0.2)
+                    play_sound('tarot1', 1.5)
+                    ease_colour(G.C.UI_CHIPS, {0.8, 0.45, 0.85, 1})
+                    ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
+                    attention_text({
+                        scale = 1.4, text = text, hold = 2, align = 'cm', offset = {x = 0,y = -2.7},major = G.play
+                    })
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        blockable = false,
+                        blocking = false,
+                        delay =  4.3,
+                        func = (function() 
+                                ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
+                                ease_colour(G.C.UI_MULT, G.C.RED, 2)
+                            return true
+                        end)
+                    }))
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        blockable = false,
+                        blocking = false,
+                        no_delete = true,
+                        delay =  6.3,
+                        func = (function() 
+                            G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] = G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
+                            G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] = G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
+                            return true
+                        end)
+                    }))
+                    return true
+                end)
+            }))
+    
+            delay(0.6)
+
+            return {
             }
         end
     end
